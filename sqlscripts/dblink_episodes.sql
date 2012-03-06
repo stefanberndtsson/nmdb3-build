47ff5c51b0b2b830be5589dd9615ae94349245b3
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2012-03-07 00:04:36 +0100

    Add a sort value column for episodes, since fetching it from release dates is too slow

--- sqlscripts/dblink_episodes.sql
+++ sqlscripts/dblink_episodes.sql
@@ -1,8 +1,19 @@
 ALTER TABLE movies ADD COLUMN parent_id INT;
+ALTER TABLE movies ADD COLUMN episode_sort_value INT;
 UPDATE movies SET parent_id = t.parent_id 
        FROM 
        (SELECT m.id AS parent_id,e.id AS episode_id FROM movies m
         INNER JOIN movies e ON e.episode_parent_title = m.full_title) t
        WHERE id = t.episode_id;
+
+CREATE OR REPLACE FUNCTION first_release_stamp(movie_id INT)
+RETURNS INT
+AS
+$$
+SELECT EXTRACT(epoch FROM MIN(release_stamp))::INT FROM release_dates WHERE movie_id = $1
+$$
+LANGUAGE SQL;
+
+UPDATE movies SET episode_sort_value = first_release_stamp(id) WHERE is_episode = true;
 
 CREATE INDEX movies_idx_parent_id ON movies (parent_id);
