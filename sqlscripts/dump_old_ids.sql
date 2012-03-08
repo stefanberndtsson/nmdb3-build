f42a9aeaee32e81737e52be4cc8fe4df18ed8b6b
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2012-03-09 00:59:31 +0100

    [Issue 1] No need to dump lines with empty imdb_ids

--- sqlscripts/dump_old_ids.sql
+++ sqlscripts/dump_old_ids.sql
@@ -6,4 +6,4 @@ SELECT id,full_title FROM movies WHERE suspended IS NULL;
 \o people_ids.dat
 SELECT id,full_name FROM people;
 \o imdb_ids.dat
-SELECT id,imdb_id FROM movies;
+SELECT id,imdb_id FROM movies WHERE imdb_id IS NOT NULL;
